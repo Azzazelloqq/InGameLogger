2d4dbfd7512b1b29d3ca7c3e43298a3f236a0489
Azzazelloqq <bog22232@ya.ru>
2024-10-19 21:37:38 +0400

    add logger

---
+++ IInGameLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InGameLogger
+{
+public interface IInGameLogger : IDisposable
+{
+	public void Log(string message);
+	public void LogError(string message);
+	public void LogError(Exception message);
+	public void LogWarning(string message);
+	public void LogException(Exception exception);
+}
+}
---
+++ UnityInGameLogger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace InGameLogger
+{
+public class UnityInGameLogger : IInGameLogger
+{
+	public void Log(string message)
+	{
+		#if UNITY_EDITOR || DEVELOPMENT_BUILD
+		Debug.Log(message);
+		#endif
+	}
+
+	public void LogError(string message)
+	{
+		#if UNITY_EDITOR || DEVELOPMENT_BUILD
+		Debug.LogError(message);
+		#endif
+	}
+
+	public void LogError(Exception message)
+	{
+		#if UNITY_EDITOR || DEVELOPMENT_BUILD
+		Debug.LogError(message);
+		#endif
+	}
+
+	public void LogWarning(string message)
+	{
+		#if UNITY_EDITOR || DEVELOPMENT_BUILD
+		Debug.LogWarning(message);
+		#endif
+	}
+
+	public void LogException(Exception exception)
+	{
+		#if UNITY_EDITOR || DEVELOPMENT_BUILD
+		Debug.LogException(exception);
+		#endif
+	}
+
+	public void Dispose()
+	{
+	}
+}
+}
